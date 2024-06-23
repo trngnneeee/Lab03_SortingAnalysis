5e4514b75ae79a0abfb2edf60686ffcf67b76701
khoavadienq <khoadangnn>
2024-06-23 19:04:36 +0700

    Flash Sort

---
+++ flashSort.h
@@ -0,0 +1,14 @@
+#pragma once 
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+int minElement(int *arr, int n, int &count_comparison);
+int maxElement(int *arr, int n, int &count_comparison);
+void flashSortComparison(int *arr, int n, int &count_comparison);
+
+void flashSort(int *arr, int n);
+void flashSortRuntime(int *arr, int n);
