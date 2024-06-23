961aecd7ca8ca3b52891da40ddfc9eb3ed8b460a
Nguyễn Trần Quốc Duy <145688546+ntqduy@users.noreply.github.com>
2024-06-23 23:05:09 +0700

    Update Merge_Sort.h

--- Merge_Sort.h
+++ Merge_Sort.h
@@ -1,7 +1,7 @@
 //merge sort function
 void mergeComparision(int *arr, int first, int mid, int last, int &comparision);
 void mergeSortComparision(int *arr, int first, int last, int &comparision);
-
+void calComparisionMergeSort(int *arr, int n, int &comparision);
 
 void mergeTime(int *arr, int first, int mid, int last);
 void mergeSortTime(int *arr, int first, int last);
